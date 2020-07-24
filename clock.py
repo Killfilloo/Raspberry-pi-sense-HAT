a43e3410adf1ea83ea4e5c7ab64be573c0c5de33
Killfill <55883896+Killfilloo@users.noreply.github.com>
2020-07-24 09:41:43 +0200

    ^

---
+++ clock.py
@@ -0,0 +1,12 @@
+﻿#Imports
+from sense_hat import SenseHat
+import time
+sense = senseHat()
+
+#Running code
+while True:
+    hour = time.localtime()[3]
+    minute = time.localtime()[4]
+    second = time.localtime()[5]
+    print(hour, minute, second)
+    sense.show_message(str(hour)+’ : ‘+str(minute)+’ : ‘+str(second), text_colour=(255,0,0), scroll_speed=0.1)
